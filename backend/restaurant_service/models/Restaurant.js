7d17db8f3d23554d5c406f5772ac51064813ed94
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-23 06:10:10 +0530

    Update Restaurant.js

---
+++ backend/restaurant_service/models/Restaurant.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const RestaurantSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
+  address: String,
+  isAvailable: { type: Boolean, default: true }
+}, { timestamps: true });
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
