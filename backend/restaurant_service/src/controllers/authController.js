9034f940e882a12595bdac62cf00f487add35461
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-23 22:13:22 +0530

    Update authController.js

--- backend/restaurant_service/src/controllers/authController.js
+++ backend/restaurant_service/src/controllers/authController.js
@@ -1,9 +1,21 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+
 exports.register = async (req, res) => {
   const { name, email, password, role } = req.body;
   const hashed = await bcrypt.hash(password, 10);
   const user = await User.create({ name, email, password: hashed, role });
   res.status(201).json(user);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '8h' });
+  res.json({ token });
 };
