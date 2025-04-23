4b5293206d65d41e693c717c1ea1ec6d75b3269b
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-23 22:17:56 +0530

    Update menuItemController.js

--- backend/restaurant_service/src/controllers/menuItemController.js
+++ backend/restaurant_service/src/controllers/menuItemController.js
@@ -11,3 +11,13 @@ exports.update = async (req, res) => {
     const item = await MenuItem.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(item);
   };
+
+  exports.remove = async (req, res) => {
+    await MenuItem.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Deleted' });
+  };
+
+  exports.list = async (req, res) => {
+    const items = await MenuItem.find({ restaurant: req.user._id });
+    res.json(items);
+  };
