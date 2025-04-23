bd5d0833318903ca725274043a6f7000ad7b284e
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-23 23:24:06 +0530

    update

--- backend/restaurant_service/src/models/Transaction.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const TransactionSchema = new mongoose.Schema({
-  restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
-  amount: Number,
-  status: { type: String, enum: ['pending','completed','failed'], default: 'pending' }
-}, { timestamps: true });
-module.exports = mongoose.model('Transaction', TransactionSchema);
