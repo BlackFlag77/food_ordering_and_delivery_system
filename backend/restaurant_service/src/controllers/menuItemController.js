bce57bfbe063cf89c1b566d3af37f13b847894fe
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-23 22:17:00 +0530

    Update menuItemController.js

--- backend/restaurant_service/src/controllers/menuItemController.js
+++ backend/restaurant_service/src/controllers/menuItemController.js
@@ -5,3 +5,9 @@ exports.add = async (req, res) => {
   const item = await MenuItem.create({ restaurant: req.user._id, ...req.body });
   res.status(201).json(item);
 };
+
+
+exports.update = async (req, res) => {
+    const item = await MenuItem.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(item);
+  };
