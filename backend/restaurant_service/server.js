a21de6293b6a38301601cee318c10b0b5fe62106
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-23 22:36:22 +0530

    Update server.js

--- backend/restaurant_service/server.js
+++ backend/restaurant_service/server.js
@@ -6,5 +6,13 @@ const app = express();
 // connect to MongoDB
 connectDB();
 
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/restaurants', restaurantRoutes);
+app.use('/api/menu', menuRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/admin', adminRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
