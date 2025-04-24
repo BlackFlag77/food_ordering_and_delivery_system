6fea5341eb32a7d5fb30b9ce21b9de207dc9aa2a
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-24 06:17:45 +0530

    remove

--- backend/restaurant_service/src/models/Restaurant.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const RestaurantSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  address: String,
-  isAvailable: { type: Boolean, default: true }
-}, { timestamps: true });
-module.exports = mongoose.model('Restaurant', RestaurantSchema);
