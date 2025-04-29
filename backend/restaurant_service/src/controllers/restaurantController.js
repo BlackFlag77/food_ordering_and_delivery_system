adf22fcd62ffa7bfe889fb918ef87d6470b7c1b9
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2025-04-29 15:06:10 +0530

    Update restaurantController.js

--- backend/restaurant_service/src/controllers/restaurantController.js
+++ backend/restaurant_service/src/controllers/restaurantController.js
@@ -1,4 +1,6 @@
 const Restaurant = require('../models/Restaurant');
+const Order = require('../models/Order');
+const mongoose = require('mongoose');
 
 // Only 'restaurant_admin' users may create restaurants
 exports.create = async (req, res, next) => {
@@ -58,4 +60,54 @@ exports.setAvailability = async (req, res, next) => {
     await r.save();
     res.json(r);
   } catch (err) { next(err); }
+};
+
+exports.getStats = async (req, res, next) => {
+  try {
+    const restaurantId = req.params.id;
+    
+    // Get total orders for this restaurant
+    const totalOrders = await Order.countDocuments({ restaurantId });
+    
+    // Get today's orders
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const todayOrders = await Order.countDocuments({
+      restaurantId,
+      createdAt: { $gte: today }
+    });
+    
+    // Get total revenue
+    const orders = await Order.find({ restaurantId });
+    const totalRevenue = orders.reduce((sum, order) => sum + order.total, 0);
+    
+    // Get popular items
+    const popularItems = await Order.aggregate([
+      { $match: { restaurantId: mongoose.Types.ObjectId(restaurantId) } },
+      { $unwind: '$items' },
+      {
+        $group: {
+          _id: '$items.menuItemId',
+          name: { $first: '$items.name' },
+          price: { $first: '$items.price' },
+          orderCount: { $sum: '$items.quantity' }
+        }
+      },
+      { $sort: { orderCount: -1 } },
+      { $limit: 5 }
+    ]);
+    
+    // Get average rating (if ratings are implemented)
+    const averageRating = 4.5; // Placeholder until ratings are implemented
+    
+    res.json({
+      totalOrders,
+      todayOrders,
+      totalRevenue,
+      averageRating,
+      popularItems
+    });
+  } catch (err) {
+    next(err);
+  }
 };
