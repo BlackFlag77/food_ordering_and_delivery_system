489434d4dc86f655bac6d077fddcaa9cfe3db165
Kavinigamalath <kavinigamalath@gmail.com>
2025-05-06 12:18:30 +0530

    change order.js routes

--- backend/order_service/src/routes/orders.js
+++ backend/order_service/src/routes/orders.js
@@ -1,17 +1,19 @@
-// src/routes/orders.js
-const express = require('express');         // ← load express
-const router  = express.Router();          // ← get Express's Router
+const express = require('express');         
+const router  = express.Router();          
 const auth = require('../middleware/auth');
 console.log('auth middleware:', auth); 
+const { permitCustomer } = require('../middleware/roles');
 const ctrl    = require('../controllers/orderController');
 
 // Protect every /api/orders route
 router.use(auth);
 
-router.post('/',           ctrl.createOrder);
+router.post('/',          permitCustomer, ctrl.createOrder);
 router.get('/',            ctrl.listOrders);
+router.get('/mine',       permitCustomer, ctrl.getMyOrders);
 router.get('/:id',         ctrl.getOrder);
 router.patch('/:id',         ctrl.updateOrder);
 router.patch('/:id/status',ctrl.patchStatus);
+
 
 module.exports = router;
